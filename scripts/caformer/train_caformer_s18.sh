89babffbd69dc729bd367e462da103f8bae0ca54
zizhao-hu <zizhaohu3@gmail.com>
2024-08-21 20:29:27 -0700

    init

--- scripts/caformer/train_caformer_s18.sh
+++ scripts/caformer/train_caformer_s18.sh
@@ -1,14 +1,14 @@
-DATA_PATH=/path/to/imagenet
-CODE_PATH=/path/to/code/metaformer # modify code path here
+DATA_PATH=~/dev/metaformer/imagenet
+CODE_PATH=~/dev/metaformer # modify code path here
 
 
 ALL_BATCH_SIZE=4096
-NUM_GPU=8
-GRAD_ACCUM_STEPS=4 # Adjust according to your GPU numbers and memory size.
+NUM_GPU=1
+GRAD_ACCUM_STEPS=128 # Adjust according to your GPU numbers and memory size.
 let BATCH_SIZE=ALL_BATCH_SIZE/NUM_GPU/GRAD_ACCUM_STEPS
 
 
-cd $CODE_PATH && sh distributed_train.sh $NUM_GPU $DATA_PATH \
+cd $CODE_PATH && ls && sh distributed_train.sh $NUM_GPU $DATA_PATH \
 --model caformer_s18 --opt lamb --lr 8e-3 --warmup-epochs 20 \
 -b $BATCH_SIZE --grad-accum-steps $GRAD_ACCUM_STEPS \
 --drop-path 0.15 --head-dropout 0.0
--- train.py
+++ train.py
@@ -54,8 +54,8 @@ from timm import utils
 from timm.data import create_dataset, create_loader, resolve_data_config, Mixup, FastCollateMixup, AugMixDataset
 from timm.loss import JsdCrossEntropy, SoftTargetCrossEntropy, BinaryCrossEntropy, \
     LabelSmoothingCrossEntropy
-from timm.models import create_model, safe_model_name, resume_checkpoint, load_checkpoint, \
-    convert_splitbn_model, convert_sync_batchnorm, model_parameters, set_fast_norm
+from timm.models import create_model, safe_model_name, resume_checkpoint, load_checkpoint,  model_parameters
+from timm.models.layers import convert_splitbn_model, convert_sync_batchnorm, set_fast_norm
 from timm.optim import create_optimizer_v2, optimizer_kwargs
 from timm.scheduler import create_scheduler
 # from timm.utils import ApexScaler, NativeScaler
